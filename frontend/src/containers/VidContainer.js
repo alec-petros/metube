5ffa16300c8fe31dd72b4905320296adc5bee5d0
alec-petros <a.petros@verizon.net>
2018-05-08 11:03:35 -0400

    nothing

--- frontend/src/containers/VidContainer.js
+++ frontend/src/containers/VidContainer.js
@@ -5,7 +5,10 @@ import Video from '../components/Video'
 
 class VidContainer extends React.Component {
 
+
+
   render() {
+    console.log(this.props)
 
     const videos = this.props.videos.map(video => {
       return (<Video auth={this.props.auth} key={video.data.attributes.handle} video={video} deleteVideo={this.props.deleteVideo} selectVideo={this.props.selectVideo} />)
